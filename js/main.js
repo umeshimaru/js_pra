134cdbb610e4462d506ffb900b90e68397b5f482
umekishimaru <riki12344649@gmail.com>
2022-06-19 11:57:54 +0900

    カルーセル

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+{
+const name = document.querySelector('button');
+const a = document.querySelector('h1');
+name.addEventListener('click',()=>{
+  
+a.textContent='umeda masanori';
+
+})
+
+
+
+
+
+
+
+
+}
